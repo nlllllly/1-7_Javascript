8c4a0bace39b760765e41a6b0c7d4c4f3770f81a
nlllllly <unfmosp.y14@gmail.com>
2020-07-12 22:04:14 +0900

    [start]1-7_Javascript

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 // $(function(){
-//     $('.box1').mouseover(function(){
-//         $('.box1').css({'background-color': '#0000FF'});
-//     });
-//     $('.box1').mouseout(function(){
-//         $('.box1').css({'background-color': '#FF0000'});
+//     $('.bg1').on('click', function(){
+//         $('.bg1').slideUp();
 //     });
 
+//     $('.bg2').on('click', function(){
+//         $('.bg2').slideUp();
+//     });
+
+//     $('.bg3').on('click', function(){
+//         $('.bg3').slideUp();
+//     });
+
+//     $('.bg4').on('click', function(){
+//         $('.bg4').slideUp();
+//     });
 // });
 
 
 
 // $(function(){
-//     $('.box1').mouseover(function(){
-//         $('.box1').addClass('box1-ext');
-//     });
-//     $('.box1').mouseout(function(){
-//         $('.box1').removeClass('box1-ext');
+//     $('.box1').on('click', function(){
+//         $(this).slideUp();
 //     });
 // });
 
-
-
 $(function(){
-    $('.box1').on('click', function(){
-        $('.box1').addClass('box1-ext');
-    });
-    $('.box1').mouseout(function(){
-        $('.box1').removeClass('box1-ext');
+    $('button').on('click', function(){
+        $('ul').children().css('color','red');
     });
 });
